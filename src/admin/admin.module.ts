6706340a596912c7a0d0914e18acd9805ac7d324
Ivan Miguel Perez Torres <ivanmperezt@gmail.com>
2025-03-17 10:38:11 -0300

    Implement: SIPR-11 - Upgrade (apis) LIKE query - Delete (bff) companies resolver - Delete (sales) multiplication cost  * qty

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,14 +3,12 @@ import { PfxHttpModule } from 'profaxnojs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AdminCompanyResolver } from './admin-company.resolver';
-import { AdminCompanyService } from './admin-company.service';
 import { AdminUserResolver } from './admin-user.resolver';
 import { AdminUserService } from './admin-user.service';
 
 @Module({
   imports: [ConfigModule, PfxHttpModule],
-  providers: [AdminCompanyResolver, AdminCompanyService, AdminUserResolver, AdminUserService],
+  providers: [AdminUserResolver, AdminUserService],
   exports: [AdminUserService]
 })
 export class AdminModule {}
